fb520f913e66fe4032d500ba71bd6023939c35f9
shivam pal <devshivampal360@gmail.com>
2024-08-09 21:34:49 +0530

    javacript part 1

---
+++ index.js
@@ -0,0 +1,72 @@
+// JavaScript (JS);
+//     >> Primitive Data Type
+//     >> Non-Primitive Data Type
+
+//     >>Number Ex: 123
+//     >>Boolean ex: true or false
+//     >>String: ex: shivam
+//     >>null: 
+//     >> undefined:
+
+
+//     var, let or const
+
+//     var name = "shivam pal"
+//     var num = 1;
+
+    // var sum = null;
+    // console.log(sum);
+
+    // var total;
+    // console.log(total);
+
+    // var name = "shiv";
+    // console.log("My Name is",name);
+
+    // var age = 21;
+
+    // console.log("My age is",age);
+
+
+
+
+// Non-Primitive Data Types:
+// >>Objects
+// >>Array
+
+// Objects:
+
+// var obj1 = {
+//     // key:value 
+//     name:"shivam",
+//     age:21,
+//     isEligibleToVote:true,
+// }
+
+// console.log(obj1);
+// console.log(obj1.age);
+// console.log(obj1.name);
+
+// Arrays
+
+// var arr1 = [123, "shivam" ,27,"pal"];
+
+// console.log(arr1);
+// console.log(arr1[2]);
+
+
+
+var data = "Shivam pal";
+// console.log(data.toLocaleLowerCase());
+// console.log(data.toUpperCase());
+// console.log(data.length);
+
+console.log(data.slice(2,6));
+
+var sentance = "JavaScripts is mind blowing";
+
+console.log(sentance);
+console.log(sentance.replace("JavaScript","Shivam"));
+console.log(sentance.includes("Pal"))
+
+
